7ddd16a77845d64e2dde37327c157c0e16ca8919
Anaya Bussey <anayabussey@gmail.com>
2024-04-18 20:57:42 -0400

    A6

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from './Lab5.js'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+import mongoose from "mongoose";
+import UserRoutes from './Users/routes.js';
 
 const app = express();
+dotenv.config();
 app.use(cors());
 app.use(express.json());
+const url = process.env.DATABASE_URL
+mongoose.connect(url);
 Lab5(app);
+UserRoutes(app);
 Hello(app)
 app.listen(4000)
